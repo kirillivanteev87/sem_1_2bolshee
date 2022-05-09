ddcb5ebaa04da126d8be6b51a131e139d4121a87
Kirill <ivanteev87@mail.ru>
2022-05-10 00:18:01 +0300

    new program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿ double a = 2, b = 3, c = 7;
+            
+                if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
